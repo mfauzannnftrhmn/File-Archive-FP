8c40c6e78c14e6156caa7676a6336d1cf0b1b453
Kakoi16 <if23.muhammadnurfadilah@mhs.ubpkarawang.ac.id>
2025-06-24 13:20:12 +0700

    memex

--- capacitor.config.ts
+++ capacitor.config.ts
@@ -1,15 +1,9 @@
 import type { CapacitorConfig } from '@capacitor/cli';
 
 const config: CapacitorConfig = {
-<<<<<<< HEAD
   appId: 'com.kakoi.app',
   appName: 'Sistem Manajemen File Archive',
   webDir: 'www'
-=======
-  appId: 'io.ionic.starter',
-  appName: 'SampleApk',
-  webDir: 'www',
->>>>>>> ee3a6cfc61840b47f99ae453aa242519fc7230d1
 };
 
 export default config;
